4d5e4080ea37154c95bf851303ce1baa165050fc
Stephen Taylor <stephenmtaylor93@gmail.com>
2025-01-21 11:23:12 -0500

    done'

--- simple_http_server/http_server.sh
+++ simple_http_server/http_server.sh
@@ -3,17 +3,34 @@
 function server () {
     while true
     do
-        read method path version
+        message_arr=()
+        check=true
+        while $check
+        do
+            read line
+            message_arr+=($line)
+            if [[ "${#line}" -eq 1 ]]
+            then
+                check=false
+            fi
+        done
+        method=${message_arr[0]}
+        path=${message_arr[1]}
         if [[ $method = 'GET' ]]
         then
             if [[ -f "./www/$path" ]]
             then
-                echo -ne 'HTTP/1.1 200 OK\r\n\r\n' ; cat "./www/$path"
+                echo -ne 'HTTP/1.1 200 OK\r\n'
+                echo -ne 'Content-Type: text/html; charset=utf-8\r\n'
+                echo -ne "Content-Length: $(wc -c < './www/'$path)\r\n\r\n" 
+                cat "./www/$path"
             else
-                echo -ne 'HTTP/1.1 404 Not Found\r\n\r\n'
+                echo -ne 'HTTP/1.1 404 Not Found\r\n'
+                echo -ne 'Content-Length: 0\r\n\r\n'
             fi
         else
-            echo -ne 'HTTP/1.1 400 Bad Request\r\n\r\n'
+            echo -ne 'HTTP/1.1 400 Bad Request\r\n'
+            echo -ne 'Content-Length: 0\r\n\r\n'
         fi
     done
 }
